115115278bc4daf231362dee2a6c8a0fd7dfdee0
Laura Cole <lauracole1900@comcast.net>
2020-12-02 14:25:36 -0700

    navbar & session components drafted

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Container from "react-bootstrap";
-import Login from "./components/login";
-import Signup from "./components/signup";
-import Navbar from "./components/navbar";
-import Profile from "./components/profile";
-import Conference from "./components/conference";
+import Container from "react-bootstrap/Container";
+// import Login from "./components/login";
+// import Signup from "./components/signup";
+// import Navbar from "./components/navbar";
+// import Profile from "./components/profile";
+// import Conference from "./components/conference";
 import Session from "./components/session";
 
 import "./App.css";
@@ -14,20 +14,20 @@ function App() {
   return (
     <div>
       <header>
-        <Navbar />
+        {/* <Navbar /> */}
       </header>
-      <body>
+      <main>
         <Router>
           <Container fluid className="mycontainer">
-            <Route exact path="/login" component={Login} />
+            {/* <Route exact path="/login" component={Login} />
             <Route exact path="/signup" component={Signup} />
-            <Route exact path="/profile/" component={Profile} />
-            <Route exact path="/conference" component={Conference} />
+            <Route exact path="/profile" component={Profile} />
+            <Route exact path="/conference" component={Conference} /> */}
             <Route exact path="/session" component={Session} />
-            <Route path="/" component={Login} />
+            {/* <Route path="/" component={Login} /> */}
           </Container>
         </Router>
-      </body>
+      </main>
     </div>
   );
 }
