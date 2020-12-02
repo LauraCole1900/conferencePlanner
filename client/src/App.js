c50e43ab4fa0f477c1cb674bec0476a5621709e3
Laura Cole <lauracole1900@comcast.net>
2020-12-02 13:28:21 -0700

    skeleton app.js code

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Container from "react-bootstrap";
+import Login from "./components/login";
+import Signup from "./components/signup";
+import Navbar from "./components/navbar";
+import Profile from "./components/profile";
+import Conference from "./components/conference";
+import Session from "./components/session";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <Navbar />
       </header>
+      <body>
+        <Router>
+          <Container fluid className="mycontainer">
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/profile/" component={Profile} />
+            <Route exact path="/conference" component={Conference} />
+            <Route exact path="/session" component={Session} />
+            <Route path="/" component={Login} />
+          </Container>
+        </Router>
+      </body>
     </div>
   );
 }
