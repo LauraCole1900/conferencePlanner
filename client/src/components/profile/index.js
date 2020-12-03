1719bab32c2a4e4e4bf6b7750fadd51172f1c626
tmessall <tmessall17@gmail.com>
2020-12-03 11:23:49 -0700

    Added top of profile page

--- client/src/components/profile/index.js
+++ client/src/components/profile/index.js
@@ -7,15 +7,29 @@ import "./style.css";
 function Profile() {
   return (
     <Container fluid className="mycontainer">
-      <Row className="row">
+      <Row className="rowMain">
         <Col lg={3} className="column info">
           <h1>Name Goes Here</h1>
-          <h3>Job Title goes Here</h3>
-          <p>Edit your information here <span class="right">Change Password</span></p>
+          <h3 className="job">Job Title goes Here</h3>
+          <div id="textbox">
+            <p id="pleft">Edit your information</p>
+            <p id="pright">Change Password</p>
+          </div>
         </Col>
         <Col lg={9} className="column stats">
-          <p>Created: X</p>
-          <p>Attending: Y</p>
+          <Container fluid className="mycontainer">
+            <Row className="row">
+              <Col  lg={3} className="d-lg-block d-md-none"></Col>
+              <Col lg={2} className="column">
+                <p id="pcreate">Created: X</p>
+              </Col>
+              <Col lg={2} className="d-lg-block d-md-none"></Col>
+              <Col lg={2} className="column">
+                <p id="pattend">Attending: Y</p>
+              </Col>
+              <Col lg={3} className="column"></Col>
+            </Row>
+          </Container>
         </Col>
       </Row>
     </Container>
