a7ad1a7193892caaded3e6ee4643ca4d8b437099
tmessall <tmessall17@gmail.com>
2020-12-04 12:16:17 -0700

    Separate conference into component and page

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,23 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Container from "react-bootstrap/Container";
-import Login from "./components/login";
-// import Navbar from "./components/navbar";
-import Profile from "./components/profile";
-import Conference from "./components/conference";
-import Session from "./components/session";
+import Login from "./pages/login";
+import Navbar from "./components/navbar";
+import Profile from "./pages/profile";
+import Conference from "./pages/conferences";
+import Session from "./pages/session";
 import CreateConference from "./components/forms/createConference";
 
 import "./App.css";
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div> 
-      <header>
-        {/* <Navbar /> */}
-      </header>
-      <main>
-        <Router>
-=======
     <div>
       <Router>
         <header>
           <Navbar />
         </header>
         <main>
->>>>>>> main
           <Container fluid className="mycontainer">
             <Route exact path={["/", "/login"]} component={Login} />
             <Route exact path="/profile/" component={Profile} />
