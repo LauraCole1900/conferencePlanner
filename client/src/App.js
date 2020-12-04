670b5b6cd49852d6bbce536180ae154074715c23
tmessall <tmessall17@gmail.com>
2020-12-04 11:15:33 -0700

    Moved some folders around

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Login from "./components/login";
-import Navbar from "./components/navbar";
+// import Navbar from "./components/navbar";
 import Profile from "./components/profile";
 import Conference from "./components/conference";
 import Session from "./components/session";
@@ -14,14 +14,14 @@ function App() {
   return (
     <div> 
       <header>
-        <Navbar />
+        {/* <Navbar /> */}
       </header>
       <main>
         <Router>
           <Container fluid className="mycontainer">
             <Route exact path={["/", "/login"]} component={Login} />
             <Route exact path="/profile/" component={Profile} />
-            <Route exact path="/conference" component={Conference} />
+            <Route exact path="/conferences" component={Conference} />
             <Route exact path="/session" component={Session} />
             <Route exact path="/create_conference" component={CreateConference} />
           </Container>
