aa292095191f508a28f9e64d5b6e9cc424674a66
Anna Battistals <anna.r.battista@gmail.com>
2020-12-04 10:17:08 -0700

    basic conference page

--- client/src/components/conference/index.js
+++ client/src/components/conference/index.js
@@ -42,6 +42,16 @@ function Conference(props) {
           </Card.Body>
         </Card>
       </Row>  
+      <Card border="primary" style={{ width: '18rem' }}>
+  <Card.Header>Featured</Card.Header>
+  <Card.Body>
+    <Card.Title>Special title treatment</Card.Title>
+    <Card.Text>
+      With supporting text below as a natural lead-in to additional content.
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card>
     </Container>
   );
 }
