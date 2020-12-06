eae8d9ed69e38636f53ba2b415ab26715a628e69
Laura Cole <lauracole1900@comcast.net>
2020-12-05 20:51:15 -0700

    update src-level index.js for heroku deployment

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 import { Auth0Provider } from "@auth0/auth0-react";
+import './index.css';
 
 ReactDOM.render(
   <Auth0Provider
-    domain="dev-9w-9zvfo.us.auth0.com"
-    clientId="aUWfgytymYIWF018xm2Xe4rJopEKkvQs"
-    redirectUri={window.location.origin}
-    
+    domain="holy-mud-6180.us.auth0.com"
+    clientId="ek0Ow28dmkKDDGj3oE5FaxkK4AjibqJO"
+    redirectUri={`${window.location.origin}/profile`}
   >
     <App />
   </Auth0Provider>,
   document.getElementById("root")
 );
-
