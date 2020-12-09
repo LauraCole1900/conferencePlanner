f72cb182f4fbaf2c5440eb1116da883573748885
Laura Cole <lauracole1900@comcast.net>
2020-12-09 11:01:08 -0700

    set timeout for session-success and added screengrabs for README

--- client/src/pages/success/sessionSuccess.js
+++ client/src/pages/success/sessionSuccess.js
@@ -1,38 +1,47 @@
-import React from "react";
+import React, { Component } from "react";
+import { Redirect } from "react-router";
 import { Link, useLocation } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 import { Container, Row, Col, Button } from "react-bootstrap";
+import ToProfileBtn from "./toProfileBtn";
+import ToConfBtn from "./toConfBtn";
 
-const SessionSuccess = () => {
-  const { user } = useAuth0();
-  const location = useLocation();
-  const urlArray = window.location.href.split("/")
-	const confId = urlArray[urlArray.length - 1]
+class SessionSuccess extends Component {
+  state = {
+    ...this.state,
+    redirect: false
+  }
 
-  return (
-    <Container fluid>
-      <Row>
-        <Col sm={2}></Col>
-        <Col sm={8}>
-          <h1>You've successfully added a session!</h1>
-          <h4>Add more now?</h4>
-        </Col>
-      </Row>
-      <Row>
-        <Col sm={3}></Col>
-        <Col sm={3}>
-          <Link to={`/add_session/${confId}`} className={location.pathname === "/add_session" ? "sessionbtn active" : "sessionbtn"}>
-            <Button type="button">Yes, add sessions!</Button>
-          </Link>
-        </Col>
-        <Col sm={3}>
-          <Link to="/profile" className={location.pathname === "/profile" ? "sessionbtn active" : "sessionbtn"}>
-            <Button type="button">Not now</Button>
-          </Link>
-        </Col>
-      </Row>
-    </Container>
-  )
+  componentDidMount() {
+    this.id = setTimeout(() => this.setState({ redirect: true }), 4000)
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.id)
+  }
+
+  render() {
+    return this.state.redirect
+      ? <Redirect to="/profile" />
+      : <Container fluid>
+        <Row>
+          <Col sm={2}></Col>
+          <Col sm={8}>
+            <h1>You've successfully added your session!</h1>
+            <h4>To edit your conference information or add more sessions now,</h4>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={5}></Col>
+          <Col sm={2}>
+            <ToProfileBtn />
+            <ToConfBtn />
+          </Col>
+          <Col sm={5}>
+          </Col>
+        </Row>
+      </Container>
+  }
 }
 
 export default SessionSuccess;
