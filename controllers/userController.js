87c8bb7b374580f181c3b449b12347c4d5026658
Laura Cole <lauracole1900@comcast.net>
2021-03-02 16:06:02 -0700

    adjusted userController and tweaked License

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,54 +4,54 @@ const db = require("../models");
 
 // Defining methods for the UsersController
 module.exports = {
-    findAll: function (req, res) {
-        db.User
-            .find({})
-            .sort({ date: -1 })
-            .then(dbModel => res.json(dbModel))
-            .catch(err => res.status(422).json(err));
-    },
-    findById: function (req, res) {
-        db.User
-            .findById(req.params.id)
-            .then(dbModel => res.json(dbModel))
-            .catch(err => res.status(422).json(err));
-    },
-    //checks if user is in DB, if not then saves
-    create: function (req, res) {
-        db.User
-            .findOne({ email: req.body.email }, function (err, user) {
-                if (err) {
-                    //handle error here
-                }
+	findAll: function (req, res) {
+		db.User
+			.find({})
+			.sort({ date: -1 })
+			.then(dbModel => res.json(dbModel))
+			.catch(err => res.status(422).json(err));
+	},
+	findById: function (req, res) {
+		db.User
+			.findById(req.params.id)
+			.then(dbModel => res.json(dbModel))
+			.catch(err => res.status(422).json(err));
+	},
+	//checks if user is in DB, if not then saves
+	create: function (req, res) {
+		db.User
+			.findOne({ email: req.body.email }, function (err, user) {
+				if (err) {
+					//handle error here
+				}
 
-                //if a user was found, that means the user's email matches the entered email
-                if (user) {
-                    var err = new Error('A user with that email has already registered. Please use a different email..')
-                    err.status = 400;
-                    return err;
-                } else {
-                    db.User
-                    .create(req.body)
-                    .then(dbModel => res.json(dbModel))
-                    .catch(err => res.status(422).json(err));
-                    
-                }
-            })
+				//if a user was found, that means the user's email matches the entered email
+				if (user) {
+					var err = new Error('A user with that email has already registered. Please use a different email..')
+					err.status = 400;
+					return err;
+				} else {
+					db.User
+						.create(req.body)
+						.then(dbModel => res.json(dbModel))
+						.catch(err => res.status(422).json(err));
 
-    },
-    update: function (req, res) {
-        db.User
-            .findOneAndUpdate({ _id: req.params.id }, req.body)
-            .then(dbModel => res.json(dbModel))
-            .catch(err => res.status(422).json(err));
-    },
-    remove: function (req, res) {
-        db.User
-            .findById({ _id: req.params.id })
-            .then(dbModel => dbModel.remove())
-            .then(dbModel => res.json(dbModel))
-            .catch(err => res.status(422).json(err));
-    }
+				}
+			})
+
+	},
+	update: function (req, res) {
+		db.User
+			.findOneAndUpdate({ _id: req.params.id }, req.body)
+			.then(dbModel => res.json(dbModel))
+			.catch(err => res.status(422).json(err));
+	},
+	remove: function (req, res) {
+		db.User
+			.findById({ _id: req.params.id })
+			.then(dbModel => dbModel.remove())
+			.then(dbModel => res.json(dbModel))
+			.catch(err => res.status(422).json(err));
+	}
 };
 
